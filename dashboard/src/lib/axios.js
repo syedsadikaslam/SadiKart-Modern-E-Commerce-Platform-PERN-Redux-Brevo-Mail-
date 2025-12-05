6a0ec140f81177028c02fb85b3706f3c9e6a158c
Md Sadik <152442843+syedsadikaslam@users.noreply.github.com>
2025-12-06 02:37:42 +0530

    Change development API base URL from 5000 to 4000

--- dashboard/src/lib/axios.js
+++ dashboard/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
-      ? "http://localhost:5000/api/v1"
+      ? "http://localhost:4000/api/v1"
       : "https://sadikart-modern-e-commerce-platform-pern.onrender.com/api/v1",
   withCredentials: true,
 });
